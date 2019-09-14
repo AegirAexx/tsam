dea34a8d4551bfc047a9ae4cd47b2ee1f9303a85
Ægir Æxx <aegir.tomasson@gmail.com>
2019-09-14 06:58:29 +0000

    Added some random throw away code

--- wip/wip.cpp
+++ wip/wip.cpp
@@ -14,38 +14,27 @@
 #include <stdio.h>
 #include <bitset>
 
-// Checksum function from the internetz
-unsigned short csum(unsigned short *ptr, int nbytes){
-    register long sum;
-    unsigned short oddbyte;
-    register short answer;
-
-    sum = 0;
-
-    while(nbytes > 1){
-        sum += *ptr++;
-        nbytes -= 2;
-    }
-
-    if(nbytes == 1){
-        oddbyte = 0;
-        *((u_char*) &oddbyte) = *(u_char*) ptr;
-        sum += oddbyte;
-    }
-
-    sum = (sum >> 16) + (sum & 0xffff);
-    sum = sum + (sum >> 16);
-    answer = (short) ~sum;
-
-    return(answer);
-}
+// Functions prototypes.
+void printArguments(int argc, char* argv[]);
 
 int main(int argc, char* argv[]){
 
-    std::cout << "Parameters to the program:" << std::endl;
-    for(int i = 0; i < argc; ++i){
-        std::cout << "#" << i << " is " << argv[i] << std::endl;
-    }
+    printArguments(argc, argv);
 
     return 0;
 }
+
+// Prints out arguments.
+void printArguments(int argc, char* argv[]){
+    std::cout << "\nArguments to the program:" << std::endl;
+    for(int i = 0; i < argc; ++i){
+        std::cout << "#" << i << " is " << argv[i] << std::endl;
+    }
+}
+
+// for(auto x: UDP_buffer) std::cout << UDP_buffer[x];
+
+// std::cout << "--------------------" << std::endl;
+
+
+
